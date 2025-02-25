889f37daeb312f2c85d7afee70a62c6de47a9d33
Thelma101 <akpata.thelma@gmail.com>
2025-02-25 16:32:09 +0100

    Mongoose integration function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,29 @@
 export const signup = async (req, res) => {
     try {
         const {fullname, username, password, confirmPassword, gender} = req.body;
+    
+        if (password != confirmPassword) {
+            return res.send({error: 'Passwords do not match'})
+        }
+
+        const user = await User.findOne({username});
+
+        if (user) {
+            return res.send({error: 'User already exists'})
+        }
+
+         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+        const newUser = new User.create({
+            fullname,
+            username,
+            password,
+            gender,
+            profilePic: gender === 'male' ? boyProfilePic : girlProfilePic,
+        })
+            await newUser.save();
+            return res.send()
     } catch (error){
         res.send(error)
     }
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-const app = express();
-
-dotenv.config();
 import authRoutes from './routes/auth.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 
-const PORT = process.env.PORT || 5000;
+const app = express();
+const PORT = process.env.PORT || 5002;
 
+dotenv.config();
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World')
+// })
 
 app.use('/api/auth', authRoutes)
-
 
 app.listen(PORT, () => {
     connectToMongoDB();
