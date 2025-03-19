d370e054e5764d2667482566a63f44ffb5839b44
Thelma101 <akpata.thelma@gmail.com>
2025-03-19 10:15:30 +0100

    message model implementation

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -7,18 +7,17 @@ const messageSchema = new mongoose.Schema({
     required: true,
   },
   receiverId: {
-    type: String,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
     required: true,
   },
   message: {
     type: String,
     required: true,
   },
-  timestamp: {
-    type: Date,
-    default: Date.now,
-  },
-})
+},
+  { timestamps: true }
+)
 
 const Message = mongoose.model('Message', messageSchema);
 
