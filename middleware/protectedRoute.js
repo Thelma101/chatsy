0595262a5bf817e61181ce513e1187acce1b8eb1
Thelma101 <akpata.thelma@gmail.com>
2025-04-02 21:42:34 +0100

    protected route config

--- middleware/protectedRoute.js
+++ middleware/protectedRoute.js
@@ -4,12 +4,16 @@ const protectedRoute = (req, res, next) => {
     try {
         const token = req.cookies.jwt;
 
-        if (token) {
-            return res.status(401).json({error: 'Unauthorized - No token provided'});
+        if (!token) {
+            return res.status(401).json({ error: 'Unauthorized - No token provided' });
         }
-        
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+        if (!decoded) {
+            return res.status(401).json({ error: 'Unauthorized - Invalid token' })
+        }
     } catch (error) {
-        
+
     }
 }
 
