e8e7a9ebd10f711d80d9fdb57390e72b349938ef
Thelma101 <akpata.thelma@gmail.com>
2025-04-23 20:25:22 +0100

    protected route update

--- middleware/protectedRoute.js
+++ middleware/protectedRoute.js
@@ -5,7 +5,7 @@ const protectedRoute = (req, res, next) => {
         const token = req.cookies.jwt;
 
         if (!token) {
-            return res.status(401).json({ error: 'Unauthorized - No token provided' });
+            return res.status(401).json({ error: 'Unauthorized - No token provided'});
         }
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
@@ -15,7 +15,6 @@ const protectedRoute = (req, res, next) => {
     } catch (error) {
         return res.status(401).json({ error: 'Unauthorized - Invalid token' })
     }
-}
 }
 
 export default protectedRoute;
